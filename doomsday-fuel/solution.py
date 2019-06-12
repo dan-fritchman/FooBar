69a8a65561db6fe1cd32440c7ffd3736a8506345
Dan Fritchman <daniel.fritchman@gmail.com>
2019-06-12 17:47:11 +0100

    Level 3.1: Doomsday Fuel/ Markov Chain

--- doomsday-fuel/solution.py
+++ doomsday-fuel/solution.py
@@ -2,69 +2,17 @@
 Doomsday Fuel
 
 A cleverly hidden Markov Chain problem.
-Shout-out https://brilliant.org/wiki/absorbing-markov-chains/ for help with the math.
+Shout-out:
+https://brilliant.org/wiki/absorbing-markov-chains/
+for help with the math.
 """
 import functools
 from fractions import Fraction
 
 
-def log(mat):
-    for row in mat:
-        print(row)
+def log(arg):
+    print(arg)
     pass
-
-
-def transposeMatrix(m):
-    return list(map(list, zip(*m)))
-
-
-def getMatrixMinor(m, i, j):
-    return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
-
-
-def get_determinant(m):
-    if len(m) == 1:
-        return m[0][0]
-    if len(m) == 2:
-        return m[0][0] * m[1][1] - m[0][1] * m[1][0]
-
-    determinant = 0
-    for c in range(len(m)):
-        determinant += ((-1) ** c) * m[0][c] * get_determinant(getMatrixMinor(m, 0, c))
-    return determinant
-
-
-def get_inverse(m):
-    if len(m) == 1:
-        return [[1 / m[0][0]]]
-    determinant = get_determinant(m)
-    if len(m) == 2:
-        return [[m[1][1] / determinant, -1 * m[0][1] / determinant],
-                [-1 * m[1][0] / determinant, m[0][0] / determinant]]
-
-    # find matrix of cofactors
-    cofactors = []
-    for r in range(len(m)):
-        cofactorRow = []
-        for c in range(len(m)):
-            minor = getMatrixMinor(m, r, c)
-            cofactorRow.append(((-1) ** (r + c)) * get_determinant(minor))
-        cofactors.append(cofactorRow)
-    cofactors = transposeMatrix(cofactors)
-    for r in range(len(cofactors)):
-        for c in range(len(cofactors)):
-            cofactors[r][c] = cofactors[r][c] / determinant
-    return cofactors
-
-
-def identity_matrix(rank):
-    assert (rank >= 1)
-    rval = []
-    for r in range(rank):
-        row = [Fraction(0, 1)] * rank
-        row[r] = Fraction(1, 1)
-        rval.append(row)
-    return rval
 
 
 def gcd(a, b):
@@ -79,22 +27,71 @@ def lcm(a, b):
     return (a * b) // gcd(a, b)
 
 
-def lcmm(*args):
-    """Return lcm of args."""
-    return functools.reduce(lcm, args)
+def transpose_matrix(m):
+    return list(map(list, zip(*m)))
 
 
-def zeroMatrix(x, y):
+def get_matrix_minor(m, i, j):
+    return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
+
+
+def get_determinant(m):
+    if len(m) == 1:
+        return m[0][0]
+    if len(m) == 2:
+        return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1) ** c) * m[0][c] * get_determinant(get_matrix_minor(m, 0, c))
+    return determinant
+
+
+def get_inverse(m):
+    if len(m) == 1:
+        return [[1 / m[0][0]]]
+    determinant = get_determinant(m)
+    if len(m) == 2:
+        return [[m[1][1] / determinant, -1 * m[0][1] / determinant],
+                [-1 * m[1][0] / determinant, m[0][0] / determinant]]
+
+    # find matrix of cofactors
+    cofactors = []
+    for r in range(len(m)):
+        cofactor_row = []
+        for c in range(len(m)):
+            minor = get_matrix_minor(m, r, c)
+            cofactor_row.append(((-1) ** (r + c)) * get_determinant(minor))
+        cofactors.append(cofactor_row)
+    cofactors = transpose_matrix(cofactors)
+    for r in range(len(cofactors)):
+        for c in range(len(cofactors)):
+            cofactors[r][c] = cofactors[r][c] / determinant
+    return cofactors
+
+
+def zero_row(n):
+    """ Row of `n` Fraction-zeroes """
+    return [Fraction(0, 1) for _ in range(n)]
+
+
+def identity_matrix(rank):
+    assert (rank >= 1)
+    rval = []
+    for r in range(rank):
+        row = zero_row(rank)
+        row[r] = Fraction(1, 1)
+        rval.append(row)
+    return rval
+
+
+def zero_matrix(x, y):
     """ Matrix full of zero-valued Fractions """
-
-    def newRow(n):
-        return [Fraction(0, 1) for _ in range(n)]
-
-    return [newRow(y) for _ in range(x)]
+    return [zero_row(y) for _ in range(x)]
 
 
 def matrix_mult(X, Y):
-    rv = zeroMatrix(len(X), len(Y[0]))
+    rv = zero_matrix(len(X), len(Y[0]))
     for i in range(len(X)):
         for j in range(len(Y[0])):
             for k in range(len(Y)):
@@ -102,16 +99,30 @@ def matrix_mult(X, Y):
     return rv
 
 
+def matrix_subtract(lhs, rhs):
+    """ Return Matrix `lhs-rhs` """
+    assert (len(lhs) == len(rhs))
+    rv = zero_matrix(len(lhs), len(lhs[0]))
+    for (x, row) in enumerate(lhs):
+        for (y, val) in enumerate(row):
+            rv[x][y] = lhs[x][y] - rhs[x][y]
+    return rv
+
+
 def state_matrices(m):
     """ Parse input `m` into Markov form """
+
     def parse_row(n, row):
-        row_type = 'trans'
-        denom = sum(row)
-        if denom == 1 and row[n] == 1:
+        """ Sort through whether a row represents a transient or terminal state,
+        and where it came from in the input-matrix order. """
+
+        if is_terminal_state(n, row):
             row_type = 'terminal'
-        if denom == 0:
             denom = 1
-            row_type = 'terminal'
+        else:
+            row_type = 'trans'
+            denom = sum(row)
+
         return dict(
             input_row_num=n,
             row_type=row_type,
@@ -120,7 +131,7 @@ def state_matrices(m):
 
     parse_results = [parse_row(k, row) for (k, row) in enumerate(m)]
 
-    # Figure out our order. Transient states all first, then terminal states.
+    # Figure out our order. All transient states first, then terminal states.
     rows_in_order = []
     for row_parse in parse_results:
         if row_parse['row_type'] == 'trans':
@@ -132,46 +143,26 @@ def state_matrices(m):
         if row_parse['row_type'] != 'trans':
             row_parse['our_row_num'] = len(rows_in_order)
             rows_in_order.append(row_parse)
-    log(rows_in_order)
 
+    # Collect a mapping from input row-numbers, to our internal row-numbers.
     row_map = {}
     for row in rows_in_order:
         if row['input_row_num'] in row_map:
-            raise Exception
+            raise Exception('Error: Already Collected Row: ' + str(row))
         row_map[row['input_row_num']] = row['our_row_num']
 
-    S = zeroMatrix(len(m), len(m))
+    # And get around to building our main matrix S, one element at a time.
+    S = zero_matrix(len(m), len(m))
     for (x, row_data) in enumerate(rows_in_order):
         m_data = m[row_data['input_row_num']]
         den = row_data['denom']
         for (y, _) in enumerate(m_data):
-            num = m_data[y]
-            S[x][row_map[y]] = Fraction(num, den)
+            S[x][row_map[y]] = Fraction(m_data[y], den)
 
+    # Break this up into Q and R, the transient and terminal-state portions.
     Q = [row[:num_trans_states] for row in S[:num_trans_states]]
     R = [row[num_trans_states:] for row in S[:num_trans_states]]
     return S, Q, R
-
-
-def matrix_subtract(lhs, rhs):
-    assert (len(lhs) == len(rhs))
-    rv = zeroMatrix(len(lhs), len(lhs[0]))
-    # Very manual matrix subtraction
-    for (x, row) in enumerate(lhs):
-        for (y, val) in enumerate(row):
-            rv[x][y] = lhs[x][y] - rhs[x][y]
-    return rv
-
-
-def format(results):
-    """ Take our preferred result-format - a list of Fractions -
-    and morph it into Google's - a list of numerators, followed by a shared denominator. """
-    result_lcm = functools.reduce(lcm, [f.denominator for f in results])
-    result_nums = [f.numerator * (result_lcm // f.denominator) for f in results]
-    rval = result_nums + [result_lcm]
-    log("RVAL")
-    log(rval)
-    return rval
 
 
 def is_terminal_state(n, row):
@@ -204,42 +195,37 @@ def solve(m):
     Iback = matrix_mult(ImQ, N)
     assert (Iback == I)
     M = matrix_mult(N, R)
+    # The first row are the probabilities from the first state, AKA state zero
+    return M[0]
 
-    log("m:")
-    log(m)
-    log("S:")
-    log(S)
-    log("Q:")
-    log(Q)
-    log("R:")
-    log(R)
-    # log(I)
-    log("ImQ:")
-    log(ImQ)
-    log("N=ImQ**-1:")
-    log(N)
-    log("M=N*R:")
-    log(M)
 
-    results = M[0]
-    return results
+def format(results):
+    """ Take our preferred result-format - a list of Fractions -
+    and morph it into Google's - a list of numerators, followed by a shared denominator. """
+    result_lcm = functools.reduce(lcm, [f.denominator for f in results])
+    result_nums = [f.numerator * (result_lcm // f.denominator) for f in results]
+    return result_nums + [result_lcm]
 
 
 def solution(m):
     if (not len(m)) or (not len(m[0])):
+        # Don't think ugly cases like this will come up. But check.
         return []
     if is_terminal_state(0, m[0]):
-        # Special case for the initial state, also being a final state
+        # Special case for the initial state also being a final state.
         results = initial_terminal_state_method(m)
     else:
         results = solve(m)
 
-    log("RESULTS")
+    log("SOLVER RESULTS: ")
     log(results)
+    fmt = format(results)
+    log("FORMATTED RESULTS: ")
+    log(fmt)
+    return fmt
 
-    return format(results)
 
-
+# Google's Test Cases
 case = [
     [0, 1, 1, 0, 0],
     [0, 0, 0, 1, 1],
@@ -259,6 +245,7 @@ case = [
 ]
 assert (solution(case) == [7, 6, 8, 21])
 
+# My Test Cases
 case = [
     [0, 1, 1, 0, 0],
     [0, 0, 1, 1, 0],
@@ -365,7 +352,6 @@ case = [
 result = [1, 1]
 assert (solution(case) == result)
 
-
 case = [
     [1, 0, 0],
     [1, 1, 1],
@@ -373,7 +359,6 @@ case = [
 ]
 result = [1, 1]
 assert (solution(case) == result)
-
 
 case = [
     [0, 511, 511, 511],
