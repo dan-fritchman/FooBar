a03800a149426026efe32d589c2163fc8abb4321
Dan Fritchman <daniel.fritchman@gmail.com>
2019-06-12 17:25:54 +0100

    Fuel/ Markov problem passing. Clean up forthcoming.

--- doomsday-fuel/solution.py
+++ doomsday-fuel/solution.py
@@ -8,8 +8,9 @@ import functools
 from fractions import Fraction
 
 
-def log(arg):
-    print(arg)
+def log(mat):
+    for row in mat:
+        print(row)
     pass
 
 
@@ -75,7 +76,7 @@ def gcd(a, b):
 
 def lcm(a, b):
     """Return lowest common multiple."""
-    return a * b // gcd(a, b)
+    return (a * b) // gcd(a, b)
 
 
 def lcmm(*args):
@@ -103,18 +104,18 @@ def matrix_mult(X, Y):
 
 def state_matrices(m):
     """ Parse input `m` into Markov form """
-
-    def den(row):
-        return max(1, sum(row))
-
     def parse_row(n, row):
         row_type = 'trans'
-        if sum(row) == 0:
+        denom = sum(row)
+        if denom == 1 and row[n] == 1:
+            row_type = 'terminal'
+        if denom == 0:
+            denom = 1
             row_type = 'terminal'
         return dict(
             input_row_num=n,
             row_type=row_type,
-            denom=den(row),
+            denom=denom,
         )
 
     parse_results = [parse_row(k, row) for (k, row) in enumerate(m)]
@@ -126,11 +127,18 @@ def state_matrices(m):
             row_parse['our_row_num'] = len(rows_in_order)
             rows_in_order.append(row_parse)
     num_trans_states = len(rows_in_order)
+
     for row_parse in parse_results:
         if row_parse['row_type'] != 'trans':
             row_parse['our_row_num'] = len(rows_in_order)
             rows_in_order.append(row_parse)
     log(rows_in_order)
+
+    row_map = {}
+    for row in rows_in_order:
+        if row['input_row_num'] in row_map:
+            raise Exception
+        row_map[row['input_row_num']] = row['our_row_num']
 
     S = zeroMatrix(len(m), len(m))
     for (x, row_data) in enumerate(rows_in_order):
@@ -138,7 +146,7 @@ def state_matrices(m):
         den = row_data['denom']
         for (y, _) in enumerate(m_data):
             num = m_data[y]
-            S[x][y] = Fraction(num, den)
+            S[x][row_map[y]] = Fraction(num, den)
 
     Q = [row[:num_trans_states] for row in S[:num_trans_states]]
     R = [row[num_trans_states:] for row in S[:num_trans_states]]
@@ -166,18 +174,35 @@ def format(results):
     return rval
 
 
-def solution(m):
+def is_terminal_state(n, row):
+    """ Boolean indication of whether row `n` represents a terminal/ final state. """
+    if sum(row) == 0:
+        return True
+    if sum(row) == 1 and row[n] == 1:
+        return True
+    return False
+
+
+def initial_terminal_state_method(m):
+    """ Break out a special side-method if the initial state is also a terminal state.  Hmph. """
+    num_final_states = 0
+    for (n, row) in enumerate(m):
+        if is_terminal_state(n, row):
+            num_final_states += 1
+    results = [Fraction(0, 1)] * num_final_states
+    results[0] = Fraction(1, 1)
+    return results
+
+
+def solve(m):
+    """ Markov-Style Solver """
     S, Q, R = state_matrices(m)
-
-    if sum(m[0]) == 0:
-        # Special case for the initial state, also being a final state
-        results = [Fraction(0, 1)] * len(R[0])
-        results[0] = Fraction(1, 1)
-        return format(results)
-
     I = identity_matrix(len(Q))
     ImQ = matrix_subtract(I, Q)
     N = get_inverse(ImQ)
+    # Double-check that!
+    Iback = matrix_mult(ImQ, N)
+    assert (Iback == I)
     M = matrix_mult(N, R)
 
     log("m:")
@@ -193,11 +218,21 @@ def solution(m):
     log(ImQ)
     log("N=ImQ**-1:")
     log(N)
-    assert (matrix_mult(ImQ, N) == I)
     log("M=N*R:")
     log(M)
 
     results = M[0]
+    return results
+
+
+def solution(m):
+    if (not len(m)) or (not len(m[0])):
+        return []
+    if is_terminal_state(0, m[0]):
+        # Special case for the initial state, also being a final state
+        results = initial_terminal_state_method(m)
+    else:
+        results = solve(m)
 
     log("RESULTS")
     log(results)
@@ -279,6 +314,13 @@ result = [1, 1]
 assert (solution(case) == result)
 
 case = [
+    [1, 1],
+    [0, 0],
+]
+result = [1, 1]
+assert (solution(case) == result)
+
+case = [
     [0, 1, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -290,8 +332,8 @@ assert (solution(case) == result)
 
 case = [
     [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
     [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
 ]
@@ -300,7 +342,7 @@ assert (solution(case) == result)
 
 case = [
     [0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 1],
+    [0, 11, 0, 0, 0, 71],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [4, 0, 0, 3, 2, 0],
@@ -323,6 +365,25 @@ case = [
 result = [1, 1]
 assert (solution(case) == result)
 
+
+case = [
+    [1, 0, 0],
+    [1, 1, 1],
+    [1, 1, 1],
+]
+result = [1, 1]
+assert (solution(case) == result)
+
+
+case = [
+    [0, 511, 511, 511],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+result = [1, 1, 1, 3]
+assert (solution(case) == result)
+
 case = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -331,3 +392,189 @@ case = [
 ]
 result = [1, 0, 1]
 assert (solution(case) == result)
+
+case = [
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+result = [0, 1, 0, 0, 1]
+assert (solution(case) == result)
+
+case = [
+    [0, 0],
+    [0, 0],
+]
+result = [1, 0, 1]
+assert (solution(case) == result)
+
+case = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+result = [1, 0, 0, 1]
+assert (solution(case) == result)
+
+case = [
+    [0],
+]
+result = [1, 1]
+assert (solution(case) == result)
+
+case = [
+    [1],
+]
+result = [1, 1]
+assert (solution(case) == result)
+
+case = [
+    [],
+]
+result = []
+assert (solution(case) == result)
+
+case = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+]
+result = [1, 0, 0, 1]
+assert (solution(case) == result)
+
+case = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+]
+result = [1, 0, 0, 1]
+assert (solution(case) == result)
+
+assert (
+        solution([
+            [0, 2, 1, 0, 0],
+            [0, 0, 0, 3, 4],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ]) == [7, 6, 8, 21]
+)
+
+assert (
+        solution([
+            [0, 1, 0, 0, 0, 1],
+            [4, 0, 0, 3, 2, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]) == [0, 3, 2, 9, 14]
+)
+
+assert (
+        solution([
+            [1, 2, 3, 0, 0, 0],
+            [4, 5, 6, 0, 0, 0],
+            [7, 8, 9, 1, 0, 0],
+            [0, 0, 0, 0, 1, 2],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]) == [1, 2, 3]
+)
+assert (
+        solution([
+            [0]
+        ]) == [1, 1]
+)
+
+assert (
+        solution([
+            [0, 0, 12, 0, 15, 0, 0, 0, 1, 8],
+            [0, 0, 60, 0, 0, 7, 13, 0, 0, 0],
+            [0, 15, 0, 8, 7, 0, 0, 1, 9, 0],
+            [23, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [37, 35, 0, 0, 0, 0, 3, 21, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [1, 2, 3, 4, 5, 15]
+)
+
+assert (
+        solution([
+            [0, 7, 0, 17, 0, 1, 0, 5, 0, 2],
+            [0, 0, 29, 0, 28, 0, 3, 0, 16, 0],
+            [0, 3, 0, 0, 0, 1, 0, 0, 0, 0],
+            [48, 0, 3, 0, 0, 0, 17, 0, 0, 0],
+            [0, 6, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [4, 5, 5, 4, 2, 20]
+)
+
+assert (
+        solution([
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [1, 1, 1, 1, 1, 5]
+)
+
+assert (
+        solution([
+            [1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [2, 1, 1, 1, 1, 6]
+)
+
+assert (
+        solution([
+            [0, 86, 61, 189, 0, 18, 12, 33, 66, 39],
+            [0, 0, 2, 0, 0, 1, 0, 0, 0, 0],
+            [15, 187, 0, 0, 18, 23, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [6, 44, 4, 11, 22, 13, 100]
+)
+
+assert (
+        solution([
+            [0, 0, 0, 0, 3, 5, 0, 0, 0, 2],
+            [0, 0, 4, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 4, 4, 0, 0, 0, 1, 1],
+            [13, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 1, 8, 7, 0, 0, 0, 1, 3, 0],
+            [1, 7, 0, 0, 0, 0, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]) == [1, 1, 1, 2, 5]
+)
